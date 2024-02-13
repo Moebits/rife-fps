736a831b7858ac5e78a70809ddb32be25b8e49eb
Tenpi <pie4chan@gmail.com>
2024-02-12 22:02:58 -0500

    fix

--- start.ts
+++ start.ts
@@ -2,7 +2,7 @@ import rife from "./rife"
 import path from "path"
 
 const start = async () => {
-    const result = await rife.interlopateGIF("./videos/gifs/input.gif", "./videos/gifs", {multiplier: 4, pngFrames: true})
+    const result = await rife.interpolateGIF("./videos/gifs/input.gif", "./videos/gifs", {multiplier: 4, pngFrames: true})
     console.log(result)
 }
 start()
